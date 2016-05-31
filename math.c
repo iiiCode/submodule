9b875bcd6c8ab96e2cebc556990e3e971a192d9c
Yunchao Chen <chenyunchao@xiaoyezi.com>
2016-05-31 16:32:21 +0800

    Add math.c
    Signed-off-by: Yunchao Chen <chenyunchao@xiaoyezi.com>

---
+++ math.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
+
